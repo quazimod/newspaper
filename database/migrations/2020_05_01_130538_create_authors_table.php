68fd1a113e9e9b32f4bd17ea788d2e318b6bdc33
quazimod <hugolight8@gmail.com>
2020-05-02 13:31:13 +0300

    Create Laravel app with Database structure

---
+++ database/migrations/2020_05_01_130538_create_authors_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAuthorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('authors', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps(0);
+            $table->string('name', 128)->unique();
+            $table->string('description', 256);
+            $table->text('site');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('authors');
+    }
+}
