a76d7884e6892c070a3be9f2627f986adaba2c81
quazimod <hugolight8@gmail.com>
2020-05-08 18:49:16 +0300

    DB Seed

---
+++ database/factories/AuthorFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Author;
+use Faker\Generator as Faker;
+
+$factory->define(Author::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'description' => $faker->text,
+        'url' => $faker->url,
+    ];
+});
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Author;
+use App\Comment;
+use App\Like;
+use App\Post;
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +16,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        factory(Author::class, 15)->create()->each(function ($author) {
+            factory(Post::class, rand(1, 3))->make()->each(
+                function ($post) use ($author) {
+                    $post->author_id = $author->id;
+                    $post->save();
+
+                    factory(User::class, rand(1, 5))->create()->each(
+                        function ($user) use ($post) {
+                            $is_liked = rand(0, 1);
+
+                            if ($is_liked) {
+                                $like = new Like;
+                                $like->post_id = $post->id;
+                                $like->user_id = $user->id;
+                                $like->save();
+                            }
+
+                            factory(Comment::class, rand(1, 3))->make()->
+                                each(function(Comment $comment) use ($user, $post) {
+                                    $comment->post_id = $post->id;
+                                    $comment->user_id = $user->id;
+                                    $comment->save();
+                            });
+                    });
+            });
+        });
     }
 }
